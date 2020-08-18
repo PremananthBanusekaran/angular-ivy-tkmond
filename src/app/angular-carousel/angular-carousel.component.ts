6e7aab9038a6ca66742b4b53995f0b41aa401e05
PremananthBanusekaran <prembkworld@gmail.com>
2020-08-18 21:48:35 +0530

    Initial commit

---
+++ src/app/angular-carousel/angular-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-carousel',
+  templateUrl: './angular-carousel.component.html',
+  styleUrls: ['./angular-carousel.component.scss']
+})
+export class AngularCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
